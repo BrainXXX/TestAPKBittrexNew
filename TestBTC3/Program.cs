534e8ce6047ca25dcc58dd6a37b520aa5f97462e
BrainXXX <xxxbrainxxx@gmail.com>
2021-07-05 13:19:31 +1000

    Update 05.07.2021

--- TestBTC3/Program.cs
+++ TestBTC3/Program.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestBTC3
 {
     class BitcoinVolume : IComparable<BitcoinVolume>
     {
         public string AltcoinName { set; get; }
-        public double Volume { get; set; }
         public double Price { get; set; }
-        public int ID { get; set; }
+        public double Volume { get; set; }
 
 
         public BitcoinVolume() { }
-        public BitcoinVolume(string AltcoinName, double Volume, double Price, int ID)
+        public BitcoinVolume(string AltcoinName, double Price, double Volume)
         {
             this.AltcoinName = AltcoinName;
-            this.Volume = Volume;
             this.Price = Price;
-            this.ID = ID;
+            this.Volume = Volume;
         }
 
         // Реализуем интерфейс IComparable<T>
@@ -38,8 +35,8 @@ namespace TestBTC3
 
         public override string ToString()
         {
-            //return String.Format("MarketName: {0}\tVolume: {1}", this.AltcoinName, this.Volume);
-            return String.Format("{0}\t{1:0.00}\t{2:0.00000000}", this.AltcoinName, this.Volume, this.Price);
+            //return String.Format("{0}\t{1:0.00000000}\t{2:0.00}", this.AltcoinName, this.Price, this.Volume);
+            return $"{AltcoinName,-20}{Price,15:0.00000000}{Volume,20:0.00}";
         }
     }
 
@@ -47,63 +44,52 @@ namespace TestBTC3
     {
         static void Main(string[] args)
         {
-            File.WriteAllLines("output.txt", getWebResponse());
-            //Console.ReadKey();
+            File.WriteAllLines("output.txt", OutputWebResponse());
         }
 
-        private static string[] getWebResponse()
+        private static string[] OutputWebResponse()
         {
             string byc = getWebResponse("https://bittrex.com/api/v1.1/public/getmarketsummaries");
 
-            int pos1 = byc.IndexOf(":[", 0);
-            int pos2 = byc.IndexOf("[", pos1);
-            int pos3 = byc.IndexOf("]", pos2);
+            int pos1 = byc.IndexOf("[{");
+            int pos2 = byc.IndexOf("]", pos1);
 
-            string[] str = byc.Substring(pos2 + 1, pos3 - pos2 - 1).Split('}');
-            string[] str2 = new string[str.Length];
+            string[] str1 = byc.Substring(pos1 + 2, pos2 - pos1 - 3).Replace(",{", "").Replace("\"", "").Split('}');
 
-            //Выбираем только маркет BTC
-            for (int i = 0, b = 0; i < str.Length; i++)
-            {
-                if (str[i].Contains("BTC-"))
-                {
-                    str2[b] = str[i].Replace(",{", "").Replace("{", "");
-                    b++;
-                }
-            }
-
-            //Удаляем пустые строки
-            str2 = str2.Where(x => x != null).ToArray();
-
-            string[] str3 = new string[str2.Length];
             string[] s = new string[13];
             List<BitcoinVolume> btc_vol = new List<BitcoinVolume>();
 
             //Убираем ненужные значения, оставляем название и объём в BTC, заполняем интерфейс IComparable
-            for (int i = 0; i < str2.Length; i++)
+            for (int i = 0; i < str1.Length; i++)
             {
-                s = str2[i].Split(',');
-                btc_vol.Add(new BitcoinVolume(s[0].Split(':')[1].Replace("\"", ""), Convert.ToDouble(s[5].Split(':')[1].Replace(".", ",")), 
-                    Convert.ToDouble(s[4].Split(':')[1].Replace(".", ",")), i));
+                s = str1[i].Split(',');
+                if (!s[5].Split(':')[1].Contains("null") && s[0].Split(':')[1].Contains("BTC-"))
+                {
+                    btc_vol.Add(new BitcoinVolume(s[0].Split(':')[1], Convert.ToDouble(s[4].Split(':')[1].Replace(".", ",")),
+                        Convert.ToDouble(s[5].Split(':')[1].Replace(".", ","))));
+                }
             }
 
             //Выполняем сортировку по убыванию
             btc_vol.Sort();
 
-            //Записываем в массив строк отсортированные значения
+            string[] str2 = new string[btc_vol.Count];
             int f = 0;
+
             foreach (BitcoinVolume a in btc_vol)
             {
-                //str3[f] = a.AltcoinName + "\t" + a.Volume;
-                str3[f] = a.ToString();
+                str2[f] = a.ToString();
                 f++;
             }
 
-            return str3;
+            return str2;
         }
 
         public static string getWebResponse(string url)
         {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
+
             // create request..
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
 
